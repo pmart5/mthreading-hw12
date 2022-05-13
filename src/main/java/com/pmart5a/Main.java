cc5bcb95a3726d6ce8d47a6c9f40ca0d2107001d
Пётр Мартынчик <pmart5a@yandex.ru>
2022-05-14 02:07:49 +0400

    Изменения в методе Main.main.

--- src/main/java/com/pmart5a/Main.java
+++ src/main/java/com/pmart5a/Main.java
@@ -2,6 +2,7 @@ package com.pmart5a;
 
 import com.pmart5a.myclasses.MyCallable;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
 
@@ -12,30 +13,26 @@ public class Main {
         Callable<String> myCallable2 = new MyCallable("Задание 2", 7);
         Callable<String> myCallable3 = new MyCallable("Задание 3", 4);
         Callable<String> myCallable4 = new MyCallable("Задание 4", 4);
+        List<Callable<String>> myCallables = List.of(myCallable1, myCallable2, myCallable3, myCallable4);
         final ExecutorService threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 // Используем методы submit() и get() для исполнения задач и получения результата.
         System.out.println("Результат использования методов submit() и get():");
-        final Future<String> task1 = threadPool.submit(myCallable1);
-        final Future<String> task2 = threadPool.submit(myCallable2);
-        final Future<String> task3 = threadPool.submit(myCallable3);
-        final Future<String> task4 = threadPool.submit(myCallable4);
-        final String resultOfTask1 = task1.get();
-        System.out.println(resultOfTask1);
-        final String resultOfTask2 = task2.get();
-        System.out.println(resultOfTask2);
-        final String resultOfTask3 = task3.get();
-        System.out.println(resultOfTask3);
-        final String resultOfTask4 = task4.get();
-        System.out.println(resultOfTask4);
+        List<Future<String>> listResultOfTasksSubmit = new ArrayList<>();
+        for (Callable<String> myCallable : myCallables) {
+            listResultOfTasksSubmit.add(threadPool.submit(myCallable));
+        }
+        for (Future<String> resultOfTaskSubmit : listResultOfTasksSubmit) {
+            System.out.println(resultOfTaskSubmit.get());
+        }
 // Используем методы invokeAll() и get() для исполнения задач и получения результата.
         System.out.println("Результат использования методов invokeAll() и get():");
-        List<Future<String>> listResultOfTasks = threadPool.invokeAll(List.of(myCallable1, myCallable2, myCallable3, myCallable4));
+        List<Future<String>> listResultOfTasks = threadPool.invokeAll(myCallables);
         for (Future<String> resultTask : listResultOfTasks) {
             System.out.println(resultTask.get());
         }
 // Используем метод invokeAny() для исполнения задач и получения результата от самой быстрой задачи.
         System.out.println("Результат использования метода invokeAny():");
-        final String resultOfTasks = threadPool.invokeAny(List.of(myCallable1, myCallable2, myCallable3, myCallable4)); //asList(task1, task2, task3, task4));
+        final String resultOfTasks = threadPool.invokeAny(myCallables);
         System.out.println(resultOfTasks);
         threadPool.shutdown();
     }
