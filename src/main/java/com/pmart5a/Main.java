f0d0e3ca657e1b8e7f409f568b2bb146a8328d73
Пётр Мартынчик <pmart5a@yandex.ru>
2022-05-17 21:35:43 +0400

    Внесены изменения в классах Main и MyCallable.

--- src/main/java/com/pmart5a/Main.java
+++ src/main/java/com/pmart5a/Main.java
@@ -8,32 +8,32 @@ import java.util.concurrent.*;
 
 public class Main {
 
+    static final int NUMBER_OF_TASK = 4;
+
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        Callable<String> myCallable1 = new MyCallable("Задание 1", 5);
-        Callable<String> myCallable2 = new MyCallable("Задание 2", 7);
-        Callable<String> myCallable3 = new MyCallable("Задание 3", 4);
-        Callable<String> myCallable4 = new MyCallable("Задание 4", 4);
-        List<Callable<String>> myCallables = List.of(myCallable1, myCallable2, myCallable3, myCallable4);
+        List<Callable<String>> myCallables = new ArrayList<>();
+        for (int i = 0; i < NUMBER_OF_TASK; i++) {
+            myCallables.add(new MyCallable("Задание " + (i + 1)));
+        }
         final ExecutorService threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 // Используем методы submit() и get() для исполнения задач и получения результата.
+        List<Future<String>> listResultOfTasks = new ArrayList<>();
         System.out.println("Результат использования методов submit() и get():");
-        List<Future<String>> listResultOfTasksSubmit = new ArrayList<>();
         for (Callable<String> myCallable : myCallables) {
-            listResultOfTasksSubmit.add(threadPool.submit(myCallable));
+            listResultOfTasks.add(threadPool.submit(myCallable));
         }
-        for (Future<String> resultOfTaskSubmit : listResultOfTasksSubmit) {
-            System.out.println(resultOfTaskSubmit.get());
+        for (Future<String> resultOfTask : listResultOfTasks) {
+            System.out.println(resultOfTask.get());
         }
 // Используем методы invokeAll() и get() для исполнения задач и получения результата.
         System.out.println("Результат использования методов invokeAll() и get():");
-        List<Future<String>> listResultOfTasks = threadPool.invokeAll(myCallables);
+        listResultOfTasks = threadPool.invokeAll(myCallables);
         for (Future<String> resultTask : listResultOfTasks) {
             System.out.println(resultTask.get());
         }
 // Используем метод invokeAny() для исполнения задач и получения результата от самой быстрой задачи.
         System.out.println("Результат использования метода invokeAny():");
-        final String resultOfTasks = threadPool.invokeAny(myCallables);
-        System.out.println(resultOfTasks);
+        System.out.println(threadPool.invokeAny(myCallables));
         threadPool.shutdown();
     }
 }
--- src/main/java/com/pmart5a/myclasses/MyCallable.java
+++ src/main/java/com/pmart5a/myclasses/MyCallable.java
@@ -1,17 +1,22 @@
 package com.pmart5a.myclasses;
 
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyCallable implements Callable<String> {
 
-    public final int DELAY_TIME = 2000;
-    private final String nameTask;
+    public static final int MAX_NUMBER_OF_MESSAGES = 7;
+    public static final int DELAY_TIME = 2000;
+    private final String taskName;
     private final int numberOfMessages;
 
+    public MyCallable(String taskName) {
+        this.taskName = taskName;
+        numberOfMessages = getRandomNumberOfMessages();
+    }
 
-    public MyCallable(String nameTask, int numberOfMessages) {
-        this.nameTask = nameTask;
-        this.numberOfMessages = numberOfMessages;
+    private int getRandomNumberOfMessages() {
+        return new Random().nextInt(MAX_NUMBER_OF_MESSAGES) + 1;
     }
 
     @Override
@@ -19,10 +24,10 @@ public class MyCallable implements Callable<String> {
         int numberOfMessagesSent = 0;
         for (int i = 0; i < numberOfMessages; i++) {
             Thread.sleep(DELAY_TIME);
-            System.out.printf("Я %s. Выполняю %s. Это %d сообщение из %d.\n", Thread.currentThread().getName(), nameTask,
+            System.out.printf("Я %s. Выполняю %s. Это %d сообщение из %d.\n", Thread.currentThread().getName(), taskName,
                     ++numberOfMessagesSent, numberOfMessages);
         }
         return String.format("Я %s. %s выполнено. Отправлено в консоль %d сообщений из %d.",
-                Thread.currentThread().getName(), nameTask, numberOfMessagesSent, numberOfMessages);
+                Thread.currentThread().getName(), taskName, numberOfMessagesSent, numberOfMessages);
     }
 }
